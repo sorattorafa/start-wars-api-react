aefa7880cc7f93550304b8f6827ef5300b55b21b
rafael_soratto <rafael.soratto@appmoove.com.br>
2020-09-19 13:14:32 -0300

    connect to api

---
+++ src/helpers/ApiHelper.js
@@ -0,0 +1,13 @@
+const API_URL = 'https://swapi.dev/api';
+
+export const doGet = (path) => {
+    const url = `${API_URL}${path}`;
+    return fetch(url)
+        .then(response => response.json())
+        .catch(err => err);
+}
+
+export const getIdFromUrl = (url) => {
+    const matches = url.match(/\/([0-9]{1,})\//);
+    return matches && matches[1] ? matches[1] : null;
+}
---
+++ src/screens/Detail/index.js
@@ -0,0 +1,19 @@
+import React, {useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { doGet } from '../../helpers/ApiHelper';
+
+const Detail = () => {
+    const {id} = useParams();
+    const [person, setPerson] = useState(null);
+    useEffect(() => {
+        doGet(`/people/${id}`).then(setPerson)
+    }, [id]);
+
+    if(!person){
+        return <h1> Loading ...</h1>
+    }
+
+    return <h1> {person.name} </h1>
+}
+
+export default Detail;
